f3c5c6968f3196104e27f3c5253d670f77fe5340
pawel <p.otrebski@smartassistant.com>
2016-02-23 14:05:52 +0100

    changed the controlllers a bit. Need to add form handling and verification for forms. Now changing the templating engines or using a hybrid of templating languages

---
+++ src/main/java/com/rails/web/RegisterController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rails.web;
+import com.rails.data.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author pawel
+ */
+@Controller
+@RequestMapping({"/register"})
+public class RegisterController {
+    
+    private PersonRepository personRepository;
+    
+    @Autowired
+    public RegisterController(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public String registerationForm(){
+        
+        return "register";
+    }
+
+    /**
+     * @return the personRepository
+     */
+    public PersonRepository getPersonRepository() {
+        return personRepository;
+    }
+
+    /**
+     * @param personRepository the personRepository to set
+     */
+    public void setPersonRepository(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+}
